11be0f3e99b6163dfd919b85896c2c809e782293
paht <paht.juangphanich@gmail.com>
2025-03-15 23:36:08 -0400

    working on autogame

--- battleship_inference.py
+++ battleship_inference.py
@@ -25,31 +25,38 @@ def load_model():
 
     model.eval()
     return model 
-
-
-def run_inference(model:torch.nn.Module,current_board:npt.NDArray,board_height:int=10,board_width:int=10)->str:
+ 
+def run_inference(model:torch.nn.Module,current_board:npt.NDArray)->str:
+    board_width  = current_board.shape[0]
+    board_length = current_board.shape[1]
+    
     board_size = current_board.shape[0]*current_board.shape[1]
     current_board = np.reshape(current_board, (1, board_size))
-    current_board = torch.tensor(current_board, dtype=torch.LongTensor).to(device)
-    target_board = torch.zeros((current_board.shape),dtype=torch.LongTensor).to(device)
+    current_board = torch.tensor(current_board, dtype=torch.long).to(device)
+    target_board = torch.zeros((current_board.shape),dtype=torch.long).to(device)
 
     bomb_index = -1
     with torch.no_grad():  # Disable gradient computation for speedup
         output = model(current_board,target_board)
-    
-    output = output.numpy()
-    bomb_indices = np.where(output==2)[1]
+        predicted_token_ids = torch.argmax(output, dim=-1).cpu()  # Shape: (batch_size, seq_length)
+
+    predicted_token_ids = predicted_token_ids.numpy()
+    bomb_indices = np.where(predicted_token_ids==2)[1]
     # Convert prediction back to matrix 
+    human_readable_bomb_locations = []
     bomb_locations = []
     for bomb_index in bomb_indices:
         row = bomb_index // board_width
         col = int(bomb_index - row * board_width)
         # convert col to letter
         row_str = chr(row+65)
-        bomb_locations.append(f"{row_str}-{col}")
-    return bomb_locations,output
+        human_readable_bomb_locations.append(f"{row_str}-{col}")
+        bomb_locations.append(bomb_index)
+    return human_readable_bomb_locations,bomb_locations,predicted_token_ids.reshape((board_length,board_width))
 
-def game_helper():
+def ai_helper():
+    """AI helps you win
+    """
     # Game configuration
     ship_sizes = [2,3,3,4,5]
     board_height = 10
@@ -74,20 +81,36 @@ def auto_game():
     ship_sizes = [2,3,3,4,5]
     board_height = 10
     board_width = 10
-
+    model = load_model()
     hits = []
     past_predictions = []
     guesses = 0     
     ship_positions = place_ships(board_height,board_width,ship_sizes)
     ship_position_indices = np.where(ship_positions == 1)[1]
-
+    hit_or_miss = ""
+    
     print("Ship positions:")
     print_board(ship_positions.reshape(board_height,board_width))
 
-    current_board = torch.from_numpy(np.zeros(shape=(1,board_height*board_width), dtype=np.int64)).type(torch.int32)  # 0 no bomb, 1 bomb, 2 hit
+    current_board = torch.from_numpy(np.zeros(shape=(board_height,board_width), dtype=np.int64)).type(torch.long)  # 0 no bomb, 1 bomb, 2 hit
     while guesses < board_height*board_width and sum(hits) < sum(ship_sizes):
-        guess, past_predictions,row,col, bomb_index= run_inference(current_board, past_predictions)
+        human_readable_bomb_locations, bomb_locations,predicted_board = run_inference(model,current_board)
+        
+        print(f"\nGuess {guesses} ")
+        print_board(current_board, predicted_board)
+        bomb_index = np.random.choice(bomb_locations)
+        
+        current_board = current_board.reshape((1,board_height*board_width))
+        index = np.where(bomb_locations == bomb_index)[0][0]
+        human_readable_bomb_index = human_readable_bomb_locations[index]
+        
         current_board[0,bomb_index] = 2 * (bomb_index in ship_position_indices) + 1 * (bomb_index not in ship_position_indices)  # 0 no bomb, 1 bomb, 2 hit
+        
+        if current_board[0,bomb_index] == 2:
+            hit_or_miss = "hit"
+        else:
+            hit_or_miss = "miss"
+        print(f"Guessed {human_readable_bomb_index} {hit_or_miss}")
         guesses += 1
     print_board(current_board.reshape(board_height,board_width))
 
